a9b0c90577c2886af3bbdcfaab804be3b28687bc
Salvatore <programmazione.moderna@gmail.com>
2026-05-31 17:14:44 +0200

    feat(crypto): implement temporary Base64 CryptoService mockup
    - Created Encrypt and Decrypt methods to simulate data offscation.
    - Set up the service interface structure before implementing AES-256 encryption.

--- Services/CryptoService.cs
+++ Services/CryptoService.cs
@@ -1,6 +1,16 @@
+using System.Text;
+
 namespace Password_Manager.Services;
 
 public class CryptoService
 {
-    
+    //Funzione che cripta la password
+    public string Encrypt(string plainText, string masterPassword)
+    {
+        if (string.IsNullOrEmpty(plainText))
+            return string.Empty;
+
+        byte[] bytes = Encoding.UTF8.GetBytes(plainText);
+        return Convert.ToBase64String(bytes);
+    }
 }
