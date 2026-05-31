4d64d346efdd318b05222c75376e652ad668341c
Salvatore <programmazione.moderna@gmail.com>
2026-05-31 16:52:20 +0200

    feat(storage): add StorageService for JSON serialization
    - Implemented SaveVault to write the credentials to a local JSON file.
    - Implemented LoadVault to handle data persistence on application startup.
    - Configured JSON formatting options for human-readable output.

--- Services/StorageService.cs
+++ Services/StorageService.cs
@@ -1,6 +1,29 @@
+using System.Text.Json;
+using Password_Manager.Models;
+
 namespace Password_Manager.Services;
 
 public class StorageService
 {
+    private readonly string _filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vault.json");
     
+    //Funzione che scrive la password nel file JSON
+    public void SaveVault(Vault vault)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true};
+        string json = JsonSerializer.Serialize(vault, options);
+        File.WriteAllText(_filepath, json);
+    }
+    
+    //Funzione che carica la password dal file JSON
+    public Vault LoadVault()
+    {
+        if (!File.Exists(_filepath))
+        {
+            return new Vault();
+        }
+
+        string json = File.ReadAllText(_filepath);
+        return JsonSerializer.Deserialize<Vault>(json) ?? new Vault();
+    }
 }
